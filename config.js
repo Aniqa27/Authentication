f5e83730a22728561762bb2ce86263054c6de90e
Aniqa27 <aniqasaleem27@gmail.com>
2026-01-31 16:05:26 +0500

    perform task

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = 'https://bbyzibuavlgtzuqfmgnj.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_-esTtGeLnk_6YD3Ih7Sg8Q_tBLmKPD4';
+const SUPABASE_URL = 'https://zoouyzujhvclmoenagqp.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_FdwKXcBs9ISqo6VL47PUsg_40E6qGbP';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
